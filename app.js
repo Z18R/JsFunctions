00140d24e0f0abbadec003f94c73d9f7a0efae6f
unknown <gray78181878@gmail.com>
2021-12-15 13:33:08 +0800

    adding js increment

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-function num (){
-    let numAll = 0;
-    for(let i = 0; i < arguments.length; i++ ){
-        numAll += arguments[i];
-    }
-    return numAll;
+// function num (){
+//     let numAll = 0;
+//     for(let i = 0; i < arguments.length; i++ ){
+//         numAll += arguments[i];
+//     }
+//     return numAll;
+// }
+// document.getElementById('demo').innerHTML = num (1,5,20,6);
+
+// function incrementValue()
+// {
+//     var value = parseInt(document.getElementById('number').value, 10);
+//     value = isNaN(value) ? 0 : value;
+//     value++;
+//     document.getElementById('number').value = value;
+// }
+let i = 0;
+function btn(){
+    document.getElementById('inc').value = ++i;
+   
 }
-document.getElementById('demo').innerHTML = num (1,5,20,6);
