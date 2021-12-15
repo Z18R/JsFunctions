dd12d9d53f29c99fd871d1f9a065f9abd8860f58
unknown <gray78181878@gmail.com>
2021-12-15 15:02:44 +0800

    adding this function

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//sum of the number
+
 // function num (){
 //     let numAll = 0;
 //     for(let i = 0; i < arguments.length; i++ ){
@@ -7,6 +9,8 @@
 // }
 // document.getElementById('demo').innerHTML = num (1,5,20,6);
 
+// increment
+
 // function incrementValue()
 // {
 //     var value = parseInt(document.getElementById('number').value, 10);
@@ -14,8 +18,23 @@
 //     value++;
 //     document.getElementById('number').value = value;
 // }
-let i = 0;
-function btn(){
-    document.getElementById('inc').value = ++i;
+// let i = 0;
+// function btn(){
+//     document.getElementById('inc').value = ++i;
    
+// this function - grabbing the value of x
+
+// let x = 2;
+// function myAmazingfunction(){
+//     return this;
+// } 
+// document.getElementById('demo').innerHTML = x;
+
+const objectName = {
+    FirstName: "Joezer",
+    LastName: "Cardoza",
+    FullName: function(){
+        return this.FirstName + " " + this.LastName;
+    }
 }
+document.getElementById("demo").innerHTML = objectName.FullName();
